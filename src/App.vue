977f0f1d58ed3e6ec8ff1ba99c39595d579c58fb
tomoya06 <peng4766@gmail.com>
2020-04-06 03:21:25 +0800

    100vh height fixed

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,54 @@
 <template>
-  <div id="app">
-    <v-app id="inspire">
-      <v-system-bar>
-        <span>TEXT CROSSING</span>
-        <v-spacer></v-spacer>
-        <span class="ml-2">{{ displayGameTime }}</span>
-      </v-system-bar>
-      <v-content>
-        <v-container fluid fill-height>
-          <v-layout>
-            <v-flex xs12 sm8 md4>
-              <v-card
-                class="elevation-12 d-flex flex-column fill-height"
+  <v-app id="inspire">
+    <v-system-bar>
+      <span>TEXT CROSSING</span>
+      <v-spacer></v-spacer>
+      <span class="ml-2">{{ displayGameTime }}</span>
+    </v-system-bar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12 d-flex flex-column fill-height">
+              <v-toolbar color="primary" class="flex-grow-0" dark flat dense>
+                <v-toolbar-title>
+                  <span>{{ heroine.name }} @ {{ island.name }}</span>
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text
+                class="flex-grow-1"
+                style="overflow-y: scroll; height: 0;"
               >
-                <v-toolbar color="primary" class="flex-grow-0" dark flat dense>
-                  <v-toolbar-title>
-                    <span>{{ heroine.name }} @ {{ island.name }}</span>
-                  </v-toolbar-title>
-                  <v-spacer></v-spacer>
-                </v-toolbar>
-                <v-card-text
-                  class="flex-grow-1"
-                  style="overflow-y: scroll; height: 0;"
-                >
-                  <div class="fill-height">
-                    <div v-for="(msg, idx) in messageList" :key="idx">
-                      {{ msg }}
-                    </div>
+                <div class="fill-height">
+                  <div v-for="(msg, idx) in messageList" :key="idx">
+                    {{ msg }}
                   </div>
-                </v-card-text>
-                <v-card-actions class="d-block px-0 pb-0">
-                  <v-divider></v-divider>
-                  <div class="d-flex justify-center flex-nowrap py-2">
-                    <span class="px-2"
-                      >LIFE: {{ heroine.health | fixedNumberFilter }}</span
-                    >
-                    <span class="px-2"
-                      >FOOD: {{ heroine.food | fixedNumberFilter }}</span
-                    >
-                  </div>
-                  <v-divider></v-divider>
-                  <div class="px-6 py-2 grey darken-3">
-                    <GameController />
-                    <v-divider class="pt-2 mt-2"></v-divider>
-                    <CommonController />
-                  </div>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-content>
-    </v-app>
-  </div>
+                </div>
+              </v-card-text>
+              <v-card-actions class="d-block px-0 pb-0">
+                <v-divider></v-divider>
+                <div class="d-flex justify-center flex-nowrap py-2">
+                  <span class="px-2"
+                    >LIFE: {{ heroine.health | fixedNumberFilter }}</span
+                  >
+                  <span class="px-2"
+                    >FOOD: {{ heroine.food | fixedNumberFilter }}</span
+                  >
+                </div>
+                <v-divider></v-divider>
+                <div class="px-6 py-2 grey darken-3">
+                  <GameController />
+                  <v-divider class="pt-2 mt-2"></v-divider>
+                  <CommonController />
+                </div>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 <script>
 import WorldController from "./controllers/WorldController";
@@ -143,5 +139,10 @@ export default {
 <style>
 html {
   overflow-y: hidden;
+}
+#inspire,
+#inspire .v-application--wrap {
+  height: 100% !important;
+  min-height: 100% !important;
 }
 </style>
